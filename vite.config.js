fd5215b61d4977e40bf987f1a636810003730758
Omer Abbasi <omerabbace@gmail.com>
2025-02-04 14:29:10 +0100

    Vite config is modified

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',  // Correct!
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',  // Correct!
+  base: '/portfolio/', // Set directly (for testing)
 });
